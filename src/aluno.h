34151108a8790c89a74d4208fc440556579ff99b
aavilela <alexandr3_@hotmail.com>
2014-12-15 21:45:44 -0200

    Comentariosgit add src/aluno.h(2)

--- src/aluno.h
+++ src/aluno.h
@@ -1,6 +1,22 @@
+/*
+ * Universidade Federal de Lavras - UFLA
+ * Algorítmos e Estruturas de Dados III
+ * Professor: Dilson Lucas Pereira
+ * Alunos: Alexandre Alencar Vilela
+ * 	   William Odair Lima
+ * Turma: 10A
+ * 
+ * Trabalho Prático: Sistema de manipulação de cadastros de alunos, indexado por Tabela Hash
+ *
+ * Compilador: GCC
+ * IDE: Nenhuma
+ * Sistema Operacional Ubuntu 14.04 LTS
+ */
+
 #ifndef ALUNOS_H
 #define ALUNOS_H
 
+// Estrutura Aluno
 typedef struct Aluno
 {
 	char Nome[15];
@@ -9,5 +25,11 @@ typedef struct Aluno
 	char Matricula[15];
 	double CRA;
 } Aluno;
+
+// Protótipos
+int EscreveAluno(Aluno*, int);
+Aluno *CarregaAluno(int);
+void ImprimeAluno(Aluno *A);
+void ImprimeTodosAlunos();
 
 #endif //ALUNOS_H
